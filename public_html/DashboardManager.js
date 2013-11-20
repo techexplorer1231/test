6ac16d3dffbc91537d15403a4e1fd0ae40b3401b
pratap krishna singh <dreamweaver1231@gmail.com>
2013-11-20 23:13:29 +0530

    Working custom messages.

--- public_html/DashboardManager.js
+++ public_html/DashboardManager.js
@@ -35,20 +35,32 @@ function getdata(parent, teen) {
     for (var i = 0; i < parent.childResources.length; i++) {
         tempParent = getFromChildCurrentResourceObj(parent.childResources[i])
         jsonObjParent.push(tempParent)
-
+    console.log(parent.childResources[i].operatorId);
     }
-    console.log(JSON.stringify(jsonObjParent));
+    //console.log(JSON.stringify(jsonObjParent));
     
     
+
+
 }
-function getFromTripsummaryObj(){
-    var resourceChild= new function(){
-        
+function getFromTripsummaryObj() {
+    var resourceChild = new function() {
+        this.geoFenceViolationCnt
+        this.curfewViolationCnt
+        this.totalPointsAfterTrip
+        this.totalBadgesAfterTrip
+        this.operatorId
+        this.tripStartDateTime
+        this.tripEndTime
+        this.distanceCovered
+        this.speedViolationCnt
+        this.tripPoints
+        this.totalCredits
     }
     return resourceChild;
 }
 function getFromChildCurrentResourceObj(childCurrentResource) {
-    
+
     var resourceParent = new function() {
         this.operatorId = childCurrentResource.operatorId;
         this.eUAAccepted = childCurrentResource.eUAAccepted;
@@ -58,5 +70,5 @@ function getFromChildCurrentResourceObj(childCurrentResource) {
         this.primaryEmailAddress = childCurrentResource.primaryEmailAddress;
     };
     return resourceParent;
-    
+
 }
