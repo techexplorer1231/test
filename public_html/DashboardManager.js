ec5b2a81550292cb3b187de3a3eb6e3018593ee0
pratap krishna singh <dreamweaver1231@gmail.com>
2013-11-20 22:56:28 +0530

    Working custom messages.

---
+++ public_html/DashboardManager.js
@@ -0,0 +1,62 @@
+$(document).ready(function(e) {
+    $.getJSON('js/user.json', function(parent) {
+        $.getJSON('js/userChild.json', function(teen) {
+            getdata(parent, teen)
+        });
+    });
+});
+
+function getdata(parent, teen) {
+    var jsonDataNoTrip = [
+        {"Operator": "Susan", "Status": 1, "Manage": "<select class='operatorRoleNoTrip'></select>"},
+        {"Operator": "Jacqueline", "Status": 1, "Manage": "<select class='operatorRoleNoTrip'></select>"},
+        {"Operator": "Clara", "Status": 1, "Manage": "<select class='operatorRoleNoTrip'></select>"},
+        {"Operator": "Elaina", "Status": 1, "Manage": "<select class='operatorRoleNoTrip'></select>"}
+    ]
+//    $("#operatorNoTrip").jsonTable({
+//        head: ['Operator', 'Status', 'Manage'],
+//        json: ['Operator', 'Status', 'Manage']
+//    });
+//
+//    $("#operatorNoTrip").jsonTableUpdate({
+//        source: jsonDataNoTrip,
+//        rowClass: "rowClass",
+//        callback: function() {
+//            $("#msg").html("Table updated at " + new Date());
+//        }
+//    });
+
+//    $.each(data, function(key, value) {
+//        
+//    });
+
+    jsonObjParent = [];
+    jsonObjChild = [];
+    for (var i = 0; i < parent.childResources.length; i++) {
+        tempParent = getFromChildCurrentResourceObj(parent.childResources[i])
+        jsonObjParent.push(tempParent)
+
+    }
+    console.log(JSON.stringify(jsonObjParent));
+    
+    
+}
+function getFromTripsummaryObj(){
+    var resourceChild= new function(){
+        
+    }
+    return resourceChild;
+}
+function getFromChildCurrentResourceObj(childCurrentResource) {
+    
+    var resourceParent = new function() {
+        this.operatorId = childCurrentResource.operatorId;
+        this.eUAAccepted = childCurrentResource.eUAAccepted;
+        this.operatorStatus = childCurrentResource.operatorStatus;
+        this.dob = childCurrentResource.dob;
+        this.primaryPhoneNumber = childCurrentResource.primaryPhoneNumber;
+        this.primaryEmailAddress = childCurrentResource.primaryEmailAddress;
+    };
+    return resourceParent;
+    
+}
